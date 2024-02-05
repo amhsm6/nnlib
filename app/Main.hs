6a8d282691a890cc5dc1117bc4adce246e952a11
amhsm6 <amhsm6@gmail.com>
2024-02-05 10:17:18 +0200

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Control.Monad
 import Control.Monad.State
+import Control.Monad.Except
+import ListT
 import qualified Data.Map as M
 
-type Action = StateT Network IO
+type Action = StateT Network (ExceptT String IO)
 
 data Network = Network { nodes :: M.Map NodeIndex Node
                        , ready :: Bool
@@ -22,34 +24,50 @@ data NodeOp = NodeOp { eval :: [Double] -> Double
 
 type NodeIndex = Int
 
-compute :: Action a -> IO a
-compute m = evalStateT m $ Network M.empty True
+compute :: Action a -> IO (Either String a)
+compute m = runExceptT $ evalStateT m $ Network M.empty True
 
-getValue :: NodeIndex -> Action Double
-getValue i = do
-    (Network nodes ready) <- get
-    let node = maybe undefined id $ M.lookup i nodes
-    if ready then
-        pure $ value node
-    else do
-        value <- eval (op node) <$> mapM getValue (children node)
-        put $ Network (M.adjust (\x -> x { value = value }) i nodes) True
-        pure value
+getNode :: NodeIndex -> Action Node
+getNode i = get >>= maybe (throwError "Illegal node") pure . M.lookup i . nodes
 
 setOp :: NodeIndex -> NodeOp -> Action ()
 setOp i op = get >>= \x -> put $ Network (M.adjust (\x -> x { op = op }) i $ nodes x) False
+
+getDerivative :: NodeIndex -> Action Double
+getDerivative = getNode >=> pure . derivative
+
+getValue :: NodeIndex -> Action Double
+getValue i = do
+    node <- getNode i
+    r <- ready <$> get
+    if r then
+        pure $ value node
+    else do
+        value <- eval (op node) <$> mapM getValue (children node)
+
+        (Network nodes _) <- get
+        put $ Network (M.adjust (\x -> x { value = value }) i nodes) True
+
+        pure value
 
 calculateDerivativesOf :: NodeIndex -> Action ()
 calculateDerivativesOf i = init >> go [i]
     where init = get >>= \(Network nodes ready) -> put $ Network (M.mapWithKey f nodes) ready
           f j node = if i == j then node { derivative = 1 } else node { derivative = 0 }
 
-          go prev = undefined {-do
-              (Network nodes _) <- get
-              forM_ prev $ \i -> children $ maybe undefined id $ M.lookup i nodes-}
+          go :: [NodeIndex] -> Action ()
+          go prevLayerIdxs = do
+              prevLayer <- mapM getNode prevLayerIdxs
+              gradients <- toList $ do
+                  prev <- fromFoldable prevLayer
+                  values <- lift $ mapM getValue $ children prev
+                  let gradients = grad (op prev) values
+                  fromFoldable $ zip (children prev) gradients
 
-getDerivative :: NodeIndex -> Action Double
-getDerivative i = get >>= pure . derivative . maybe undefined id . M.lookup i . nodes
+              pure ()
+
+              {-forM_ (zip (children prev) gradients) $ \(i, g) -> do
+                  pure ()-}
 
 node :: NodeOp -> [NodeIndex] -> Action NodeIndex
 node op children = do
